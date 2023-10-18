989dd847ef7b6e3dfa2d31e8109d4ecd284b7d54
Habou Zakaria <habouzakaria@gmail.com>
2023-10-18 12:08:35 -0700

    sql more queries fix

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,16 +1,20 @@
--- Script that lists genres that not belong to Dexter show
-SELECT tv_genres.name -- Query to get genres that not belong
-FROM tv_genres
-LEFT JOIN
-(
-	SELECT tv_genres.id, tv_genres.name -- Query to get Dexter genres
-	FROM tv_genres
-	JOIN tv_show_genres
-	     ON tv_genres.id = tv_show_genres.genre_id
-	JOIN tv_shows
-	     ON tv_show_genres.show_id = tv_shows.id
-	WHERE tv_shows.title = "Dexter"
-	ORDER BY tv_genres.id
-) dexter_genres ON dexter_genres.id = tv_genres.id
-WHERE dexter_genres.id is NULL
-ORDER BY tv_genres.name;
+-- Lists all shows without the comedy genre in the database hbtn_0d_tvshows.
+-- Records are ordered by ascending show title.
+SELECT DISTINCT `title`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON s.`show_id` = t.`id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE t.`title` NOT IN
+             (SELECT `title`
+                FROM `tv_shows` AS t
+	             INNER JOIN `tv_show_genres` AS s
+		     ON s.`show_id` = t.`id`
+
+		     INNER JOIN `tv_genres` AS g
+		     ON g.`id` = s.`genre_id`
+		     WHERE g.`name` = "Comedy")
+ ORDER BY `title`;
+ 
